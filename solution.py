fb0297cb92122eb623e412b24edc8f74cb13d517
swapnith8 <kswapnith@msitprogram.net>
2020-07-13 14:03:36 +0530

    Jul 13  14:03:36

--- solution.py
+++ solution.py
@@ -1,7 +1,11 @@
 # Write a program determine whether the given number is Armstrong number or not.
 # The program should return true or false
-
 def checkArmstrong(num):
         # Your code goes here
-
-       return True
+        s = 0
+        temp = num
+        while temp > 0:
+                rem = temp % 10
+                s += rem ** 3
+                temp = temp // 10
+        return num == s
--- test_solution.py
+++ test_solution.py
@@ -4,8 +4,6 @@ from solution import checkArmstrong
 import pytest
 
 
-@pytest.mark.parametrize('x,result',[
-    (153,True),(1000,False),(370,True),(371,True),(420,False),(407,True)
-])
+@pytest.mark.parametrize('x,result',[(153,True),(1000,False),(370,True),(371,True),(420,False),(407,True)])
 def test_checkArmstrong(x,result):
     assert checkArmstrong(x) == result
